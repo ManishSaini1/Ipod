b560dca51fb8b411b75d40253b14cfdbc9001a11
Manish Saini <mk81895@gmail.com>
2020-08-11 00:15:18 +0530

    Updates

---
+++ precache-manifest.e6338218d56f96307290b45cbd82437c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e888188ac128f55ab2c9494793431b5",
+    "url": "/Ipod/index.html"
+  },
+  {
+    "revision": "a6f84aab365cfa46b3a2",
+    "url": "/Ipod/static/css/main.e5d2ce30.chunk.css"
+  },
+  {
+    "revision": "1e964757d251fbcaf16c",
+    "url": "/Ipod/static/js/2.96943f7c.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/Ipod/static/js/2.96943f7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f84aab365cfa46b3a2",
+    "url": "/Ipod/static/js/main.52a66440.chunk.js"
+  },
+  {
+    "revision": "3cd3cea432dbaf2b85de",
+    "url": "/Ipod/static/js/runtime-main.f181fb6e.js"
+  },
+  {
+    "revision": "f58a59fabd76a1dfe6ee9f67879ef19c",
+    "url": "/Ipod/static/media/Laung Gwacha.f58a59fa.mp3"
+  },
+  {
+    "revision": "13b23bf93a624c63329e4c8435d741a2",
+    "url": "/Ipod/static/media/Let Me Love You.13b23bf9.mp3"
+  },
+  {
+    "revision": "42cb592fe70552deba360bc6b8554101",
+    "url": "/Ipod/static/media/Shape of you.42cb592f.mp3"
+  },
+  {
+    "revision": "a4fb5517ac4ae3f854a509769d1c5ef3",
+    "url": "/Ipod/static/media/laung_gwacha.a4fb5517.jpg"
+  },
+  {
+    "revision": "8f8a38800f3103985ab9edb2d3cc3810",
+    "url": "/Ipod/static/media/music.8f8a3880.jpeg"
+  },
+  {
+    "revision": "220ba5c7b2ca6df36e1c8dc54c43e0fb",
+    "url": "/Ipod/static/media/shape_of_you.220ba5c7.png"
+  }
+]);
